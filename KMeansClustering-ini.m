5890f388d613c7ee2dcf75c236d702b72e83f626
Dennis-Donguk <dennis.donguk.lee@gmail.com>
2020-11-24 14:03:00 -0800

    main fn & read img code

---
+++ KMeansClustering-ini.m
@@ -0,0 +1,11 @@
+% inImgPath = 'in\autumn-leaves.jpg';
+inImgPath = 'in\cocker-spaniel.jpg';
+% inImgPath = 'in\pumpkin.jpg';
+% inImgPath = 'in\Raincoat.jpg';
+
+outPath = 'out\out-k3'
+
+% for 3 <= k <= 10
+numK = 3;
+
+KMeansClustering(inImgPath,outPath,numK);
---
+++ KMeansClustering.m
@@ -0,0 +1,11 @@
+function KMeansClustering(inImgPath,outPath,numK)
+
+    rgb = imread(inImgPath);
+
+    r = rgb(:,:,1);
+    g = rgb(:,:,2);
+    b = rgb(:,:,3);
+
+
+
+end
