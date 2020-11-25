75becc6c13616e07432a5f2236bd069ac9283b53
Dennis-Donguk <dennis.donguk.lee@gmail.com>
2020-11-24 17:32:44 -0800

    init centeroids from rand pixel

--- KMeansClustering.m
+++ KMeansClustering.m
@@ -1,10 +1,20 @@
 function KMeansClustering(inImgPath,outPath,numK)
 
+    % read img
     rgb = imread(inImgPath);
-
     r = rgb(:,:,1);
     g = rgb(:,:,2);
     b = rgb(:,:,3);
+    sz = size(rgb);
+
+    % get centeroids
+    cent = zeros(numK,3);
+    for i = 1:numK
+        randCol = round(rand*sz(1));
+        randRow = round(rand*sz(2));
+        cent(i,:) = [r(randCol,randRow) g(randCol,randRow) b(randCol,randRow)];
+    end
+
 
 
 
